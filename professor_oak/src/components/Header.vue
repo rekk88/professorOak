7a28845aff1609c8027a8f6fad0ea8e10b6ba014
m3gr4l0 <m3gr4l0@users.noreply.github.com>
2021-08-09 10:44:32 +0200

    edit: indent

--- professor_oak/src/components/Header.vue
+++ professor_oak/src/components/Header.vue
@@ -1,11 +1,10 @@
 <template>
 <div class="container p-5">
   <nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <span class="navbar-brand mb-0 h1">Navbar</span>
-  </div>
-</nav>
-  
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">Navbar</span>
+    </div>
+  </nav>
 </div>
 </template>
 
