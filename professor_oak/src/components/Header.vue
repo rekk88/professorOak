83f8b5d1de6b2f6d290a05281745a84c4324d570
m3gr4l0 <m3gr4l0@users.noreply.github.com>
2021-08-09 11:19:29 +0200

    edit: navbar

--- professor_oak/src/components/Header.vue
+++ professor_oak/src/components/Header.vue
@@ -1,11 +1,26 @@
 <template>
-<div class="container p-5">
-  <nav class="navbar navbar-light bg-light">
-    <div class="container-fluid">
-      <span class="navbar-brand mb-0 h1">Navbar</span>
+  <div class="container p-5">
+    <div class="row">
+      <div class="col-lg-6">
+        <nav class="my_nav navbar">
+          <div class="container-fluid">
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search a Pokémon" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+      </div>
+
+      <div class="col-lg-6">
+        <nav class="my_nav navbar">
+          <div class="container-fluid">
+            <span class="navbar-text">Gotta catch 'em all!</span>
+          </div>
+        </nav>
+      </div>
     </div>
-  </nav>
-</div>
+  </div>
 </template>
 
 
@@ -20,5 +35,13 @@ export default {
 <style lang="scss" scoped>
 .debug {
   color: var(--red);
+}
+
+.my_nav {
+  background-color: var(--transparent-grey);
+  backdrop-filter: blur(5px);
+
+  border: 1px solid var(--transparent-grey);
+  border-radius: 6px;
 }
 </style>
