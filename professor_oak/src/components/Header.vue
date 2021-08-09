3db4d2f95b65e021007f31cca35fab056442a78d
m3gr4l0 <m3gr4l0@users.noreply.github.com>
2021-08-09 10:41:04 +0200

    edit: header

--- professor_oak/src/components/Header.vue
+++ professor_oak/src/components/Header.vue
@@ -1,16 +1,7 @@
 <template>
-<<<<<<< HEAD
 <div>
   <div class="text-center mb-5 debug">Home test</div>
 </div>
-
-=======
-<div class="container">
-  <div class="text-center debug">Home test</div>
-
-</div>
-  
->>>>>>> 6700c534824de79aa107c6049274c377fe37502e
 </template>
 
 
@@ -26,5 +17,4 @@ export default {
 .debug {
   color: var(--red);
 }
-
 </style>
