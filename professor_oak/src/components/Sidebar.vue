9ca63caf8b891725575a993af3cd32ec58550acd
m3gr4l0 <m3gr4l0@users.noreply.github.com>
2021-08-10 12:09:38 +0200

    edit: spaces 2

--- professor_oak/src/components/Sidebar.vue
+++ professor_oak/src/components/Sidebar.vue
@@ -1,42 +1,42 @@
 <template>
   <div class="sidebar_wrap py-2 px-3">
-      <ul class="pokemon_list ps-0">
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li class="pokemon_list_item ps-2">#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-          <li>#10 ditto</li>
-      </ul>
+    <ul class="pokemon_list ps-0">
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li class="pokemon_list_item ps-2">#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+      <li>#10 ditto</li>
+    </ul>
   </div>
 </template>
 
