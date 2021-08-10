31402a2f79092b6fbb40a98e009ce5138d59297a
m3gr4l0 <m3gr4l0@users.noreply.github.com>
2021-08-10 10:29:33 +0200

    edit: type

--- professor_oak/src/components/Card.vue
+++ professor_oak/src/components/Card.vue
@@ -1,27 +1,28 @@
 <template>
- 
-<div class="card poke_card container border-0" style="width: 18rem;">
-  <img src="../assets/images/38.png" class="card-img-top rounded-circle" alt="sprite">
-  <div class="card-body">
-    <h5 class="card-title">
+  <div class="card poke_card container border-0" style="width: 18rem;">
+    <img src="../assets/images/38.png" class="card-img-top rounded-circle" alt="sprite">
+    <div class="card-body">
+      <h5 class="card-title">
         <!-- pokemon id and name here -->
         <span class="pokemon_number">#20</span> <span class="pokemon_name">Fox</span>
-    </h5>
-    <h6 class="pokemon_type fire">
-      <span class="">Fire</span>
-    </h6>
+      </h5>
+      
+      <!-- type here -->
+      <h6 class="pokemon_type flying">Fire</h6>
 
-    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a> -->
+      <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a> -->
+    </div>
   </div>
-</div>
 </template>
+
 
 <script>
 export default {
  name: 'Card',
 }
 </script>
+
 
 <style lang="scss" scoped>
 .poke_card {
@@ -30,7 +31,7 @@ export default {
   color: var(--white);
   background-color: transparent;
 
-  .card-img-top{
+  .card-img-top {
     background-color: var(--transparent-grey);
     backdrop-filter: blur(5px);
 
@@ -39,15 +40,17 @@ export default {
     box-shadow: -1px 1px 5px 9px var(--ultra-transparent-grey);
     padding: 15px;
   }
-  .card-body{
-      .card-title{
-        font-weight: 600;
-        .pokemon_number{
-          color: var(--grey);
-        }
-      }
-  }
-  padding: 0px 44px;
 
+  .card-body {
+    .card-title{
+      font-weight: 600;
+      
+      .pokemon_number{
+        color: var(--grey);
+      }
+    }
+  }
+
+  padding: 0px 44px;
 }
 </style>
