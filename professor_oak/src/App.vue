284a06327a1fff6c9165ddc21b806d86e5439e54
maicol <maicol.matteoli@gmail.com>
2021-08-11 11:03:15 +0200

    add : pokemon names getter

--- professor_oak/src/App.vue
+++ professor_oak/src/App.vue
@@ -7,13 +7,42 @@
 
 <script>
 import Home from './components/Home.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
 
   components: {
-    Home
-  }
+    Home,
+  
+  },
+  data() {
+    return {
+      x : 0,
+      pokemon : 'https://pokeapi.co/api/v2/',
+      // pokemonList : pokemon + 'pokemon?limit=898',
+      pokemonNames : [],
+      names : [],
+    }
+  },
+  created() {
+    console.log("created");
+    console.log("pokemon : " , this.pokemon);
+    axios
+      .get(this.pokemon + 'pokemon?limit=898')
+      .then(response =>{
+        console.log(response.data.results); //object {name , url}
+        this.names = response.data.results;
+        this.names.forEach(element => {
+          this.pokemonNames.push(element.name);
+          
+        });
+        console.log("pokemon list : " , this.pokemonNames);
+      })
+  },
+  methods: {
+      
+  },  
 }
 </script>
 
