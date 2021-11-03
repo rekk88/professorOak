e6a947ca7a3e1530c617e23b68b4abd20f68cdd2
maicol <maicol.matteoli@gmail.com>
2021-11-03 15:43:01 +0100

    test2

--- professor_oak/deploy.sh
+++ professor_oak/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:rekk88/professor_oak.git master:gh-pages
+git push -f git@github.com:rekk88/professorOak.git master:gh-pages
 cd -
